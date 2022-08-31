2395bc5ad2f9f165ecc30ef096697ba370730108
Tiago Duarte <tmpduarte@gmail.com>
2022-08-31 16:34:27 +0000

    add failure example

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 interface ButtonProps {
   text: string;
+  disabled?: boolean;
 }
 
-export const Button = ({ text = "Button" }: ButtonProps) => {
-  return <button type="button">{text}</button>;
+export const MyButton = ({ text = "Button", disabled }: ButtonProps) => {
+  return <button type="button">{text}{disabled}</button>;
 };
